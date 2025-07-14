913678b41365d621f3bef7dce7212dd3de8f5496
WeskleyReis <weskleyff88004216@gmail.com>
2025-07-14 19:16:58 -0300

    Pagina de registro.

--- contact/forms.py
+++ contact/forms.py
@@ -1,16 +1,24 @@
 from . import models
 from django import forms
 from django.core.exceptions import ValidationError
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
 
 class ContactForm(forms.ModelForm):
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        
+    picture = forms.ImageField(
+        widget=forms.FileInput(
+            attrs={
+                'accept': 'image/*',
+            }
+        )
+    )
+
     class Meta:
         model = models.Contact
         fields = (
             'first_name', 'last_name', 'phone',
             'email', 'description', 'category',
+            'picture',
         )
 
 
@@ -29,3 +37,38 @@ class ContactForm(forms.ModelForm):
             self.add_error('last_name', msg)
 
         return super().clean()
+    
+
+class RegisterForm(UserCreationForm):
+    first_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+
+    last_name = forms.CharField(
+        required=True,
+        min_length=3,
+    )
+
+    email = forms.EmailField()
+    
+
+    class Meta:
+        model = User
+        fields = (
+            'first_name', 'last_name', 'email',
+            'username', 'password1', 'password2'
+        )
+
+    
+    def clean_email(self):
+
+        email = self.cleaned_data.get('email')
+
+        if User.objects.filter(email=email).exists():
+            self.add_error(
+                'email',
+                ValidationError('Já existe este e-mail', code='invalid')
+            )
+
+        return email
